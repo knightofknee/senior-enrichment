615481a24fc261c3505332c569443342d5fe2f36
knightofknee <bricarlis@gmail.com>
2017-07-12 00:06:41 -0500

    end of first night, server routes done not tested

---
+++ seed.js
@@ -0,0 +1,50 @@
+const db = require('./db');
+const Student = require('./db/models/student');
+const Campus = require('./db/models/campus');
+const User = require('./db/models/user');
+
+const students = [
+  { name: 'Bri-gui' },
+];
+
+const campuses = [
+  { name: 'Carl-dom' },
+];
+
+const users = [
+  { name: 'bsizzle' },
+];
+
+
+const seed = () =>
+  Promise.all(students.map(student =>
+    Student.create(student))
+  )
+  .then(() =>
+  Promise.all(campuses.map(campus =>
+    Campus.create(campus))
+  ))
+  .then(() =>
+  Promise.all(users.map(user =>
+    User.create(user))
+  )
+);
+
+const main = () => {
+  console.log('Syncing db...');
+  db.sync({ force: true })
+    .then(() => {
+      console.log('Seeding databse...');
+      return seed();
+    })
+    .catch(err => {
+      console.log('Error while seeding');
+      console.log(err.stack);
+    })
+    .then(() => {
+      db.close();
+      return null;
+    });
+};
+
+main();
--- server/api.js
+++ server/api.js
@@ -1,10 +1,62 @@
 'use strict'
 const api = require('express').Router()
-const db = require('../db')
+const { Student, Campus } = require('../db/models')
 
 // If you aren't getting to this object, but rather the index.html (something with a joke) your path is wrong.
 	// I know this because we automatically send index.html for all requests that don't make sense in our backend.
 	// Ideally you would have something to handle this, so if you have time try that out!
 api.get('/hello', (req, res) => res.send({hello: 'world'}))
+
+api.get('/students', (req, res, next) => {
+	Student.findAll()
+	.then(students => res.json(students))
+	.catch(next)
+})
+api.get('/students/:id', (req, res, next) => {
+	Student.findById(req.params.id)
+	.then(student => res.json(student))
+	.catch(next)
+})
+api.post('/students', (req, res, next) => {
+	Student.create(req.body)
+	.then(student => res.json(student))
+	.catch(next)
+})
+api.put('/students/:id', (req, res, next) => {
+	Student.findById(req.params.id)
+	.then(student => student.update(req.body))
+	.catch(next)
+})
+api.delete('/students/:id', (req, res, next) => {
+	Student.destroy({where: {id: req.params.id}})
+	.then(()=> res.status(204).end())
+	.catch(next)
+})
+
+
+api.get('/campuses', (req, res, next) => {
+	Campus.findAll()
+	.then(campuses => res.json(campuses))
+	.catch(next)
+})
+api.get('/campuses/:id', (req, res, next) => {
+	Campus.findById(req.params.id)
+	.then(campus => res.json(campus))
+})
+api.post('/campuses', (req, res, next) => {
+	Campus.create(req.body)
+	.then(campus => res.json(campus))
+	.catch(next)
+})
+api.put('/campuses/:id', (req, res, next) => {
+	Campus.findById(req.params.id)
+	.then(campus => campus.update(req.body))
+	.catch(next)
+})
+api.delete('/campuses/:id', (req, res, next) => {
+	Campus.destroy({where: {id: req.params.id}})
+	.then(()=> res.status(204).end())
+	.catch(next)
+})
 
 module.exports = api
